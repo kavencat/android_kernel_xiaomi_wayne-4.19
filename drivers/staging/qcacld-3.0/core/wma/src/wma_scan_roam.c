6729ccd22b89d2ab7e3ff3dca0dc0762f0ae57ae
kavencat <shi772425803@163.com>
2024-12-20 21:13:24 +0800

    qcacld-3.0: wma: Fix use of undeclared identifier i,num_allowed_authmode
    Change-Id: I4b5044e5db007578985cb9bb14bb12f896d29d77
    Signed-off-by: kavencat <shi772425803@163.com>

--- drivers/staging/qcacld-3.0/core/wma/src/wma_scan_roam.c
+++ drivers/staging/qcacld-3.0/core/wma/src/wma_scan_roam.c
@@ -1064,7 +1064,7 @@ wma_roam_scan_fill_ap_profile(struct roam_offload_scan_req *roam_req,
 	uint16_t rsn_caps;
 	bool peer_rmf_capable = false;
 	uint32_t num_allowed_authmode = 0;
-	enum wlan_crypto_key_mgmt i;
+	int i;
 
 	qdf_mem_zero(profile, sizeof(*profile));
 	if (!roam_req) {
